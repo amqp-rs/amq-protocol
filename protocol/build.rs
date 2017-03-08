bf36d66cda67a68e727347f6683ee56387e2767a
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-03-08 11:39:28 +0100

    protocol: update to codegen 0.9.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/build.rs
+++ protocol/build.rs
@@ -2,31 +2,22 @@ extern crate amq_protocol_codegen;
 
 use amq_protocol_codegen::*;
 
+use std::collections::BTreeMap;
 use std::env;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::Path;
 
-fn load_template(name: &str) -> String {
-    let mut tpl = String::new();
-    std::fs::File::open(format!("templates/{}.tpl", name)).expect(&format!("Failed to open {} template", name)).read_to_string(&mut tpl).expect(&format!("Failed to read {} template", name));
-    tpl
-}
-
 fn main() {
-    let out_dir   = env::var("OUT_DIR").expect("OUT_DIR is not defined");
-    let dest_path = Path::new(&out_dir).join("protocol.rs");
-    let mut f     = File::create(&dest_path).expect("Failed to create protocol.rs");
-    let specs     = AMQProtocolDefinition::load();
-    let templates = AMQPTemplates {
-        main:     load_template("main"),
-        domain:   load_template("domain"),
-        constant: load_template("constant"),
-        klass:    load_template("class"),
-        method:   load_template("method"),
-        argument: load_template("argument"),
-        property: load_template("property"),
-    };
+    let out_dir     = env::var("OUT_DIR").expect("OUT_DIR is not defined");
+    let dest_path   = Path::new(&out_dir).join("protocol.rs");
+    let mut f       = File::create(&dest_path).expect("Failed to create protocol.rs");
+    let specs       = AMQProtocolDefinition::load();
+    let mut codegen = CodeGenerator::new().register_amqp_helpers();
+    let mut data    = BTreeMap::new();
 
-    writeln!(f, "{}", specs.code_generator(templates).generate()).expect("Failed to generate protocol.rs");
+    codegen.register_template_string("main", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/main.rs")).to_string()).expect("Failed to register main template");
+    data.insert("protocol".to_string(), specs);
+
+    writeln!(f, "{}", codegen.render("main", &data).expect("Failed to render main template")).expect("Failed to generate protocol.rs");
 }
