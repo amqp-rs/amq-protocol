90dd65cde94ca051ac8965b31b20863103efccf8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-03-22 23:52:57 +0100

    protocol: import nom and cookie-factory
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,5 +1,7 @@
 extern crate amq_protocol_codegen;
 extern crate amq_protocol_types;
+#[macro_use] extern crate cookie_factory;
+#[macro_use] extern crate nom;
 
 pub mod codegen;
 pub mod protocol;
