8cae40112d85fcdf978f7388facba1e24331feab
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-03-22 12:13:26 +0100

    codegen: rework snake_case
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- codegen/src/util.rs
+++ codegen/src/util.rs
@@ -18,6 +18,21 @@ pub fn snake_case(name: &str) -> String {
     match name {
         "type"   => "amqp_type".to_string(),
         "return" => "amqp_return".to_string(),
-        name     => name.replace("-", "_").to_lowercase(),
+        name     => {
+            let mut new_word: bool = false;
+            name.chars().fold("".to_string(), |mut result, ch| {
+                if ch == '-' || ch == '_' || ch == ' ' {
+                    new_word = true;
+                    result
+                } else {
+                    if new_word || (result.len() > 0 && ch.is_uppercase()) {
+                        result.push('_');
+                        new_word = false;
+                    }
+                    result.push(ch.to_ascii_lowercase());
+                    result
+                }
+            })
+        }
     }
 }
