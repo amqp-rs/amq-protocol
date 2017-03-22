51a259c5a248efd4444525b82b4c8276fea7dec0
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-03-22 22:49:44 +0100

    protocol: add constants
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/templates/main.rs
+++ protocol/templates/main.rs
@@ -1,6 +1,12 @@
-pub const NAME:          &'static str = "{{protocol.name}}";
-pub const MAJOR_VERSION: u8           = {{protocol.major_version}};
-pub const MINOR_VERSION: u8           = {{protocol.minor_version}};
-pub const REVISION:      u8           = {{protocol.revision}};
-pub const PORT:          u32          = {{protocol.port}};
-pub const COPYRIGHT:     &'static str = r#"{{copyright}}"#;
+use types::*;
+
+pub const NAME:          &'static str   = "{{protocol.name}}";
+pub const MAJOR_VERSION: ShortShortUInt = {{protocol.major_version}};
+pub const MINOR_VERSION: ShortShortUInt = {{protocol.minor_version}};
+pub const REVISION:      ShortShortUInt = {{protocol.revision}};
+pub const PORT:          LongUInt       = {{protocol.port}};
+pub const COPYRIGHT:     &'static str   = r#"{{copyright}}"#;
+
+{{#each protocol.constants as |constant| ~}}
+pub const {{sanitize_name constant.name}}: {{constant.type}} = {{constant.value}};
+{{/each ~}}
