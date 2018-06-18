4eb54753d55eb544895628f3ea96d7479469724d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-06-18 16:09:17 +0200

    types: document types
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- types/src/types.rs
+++ types/src/types.rs
@@ -3,30 +3,54 @@ use value::AMQPValue;
 use std::collections::BTreeMap;
 use std::fmt;
 
+///! Enumeration referencing all the available AMQP types
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub enum AMQPType {
+    ///! A bool
     Boolean,
+    ///! An i8
     ShortShortInt,
+    ///! A u8
     ShortShortUInt,
+    ///! An i16
     ShortInt,
+    ///! A u16
     ShortUInt,
+    ///! An i32
     LongInt,
+    ///! A u32
     LongUInt,
+    ///! An i64
     LongLongInt,
+    ///! A u64
     LongLongUInt,
+    ///! An f32
     Float,
+    ///! An f64
     Double,
+    ///! A decimal value represented by a scale and a value
     DecimalValue,
+    ///! Deprecated, a String
     ShortString,
+    ///! A String
     LongString,
+    ///! An array of AMQPValue
     FieldArray,
+    ///! A timestamp (u32)
     Timestamp,
+    ///! A Map<String, AMQPValue>
     FieldTable,
+    ///! An array of bytes, RabbitMQ specific
     ByteArray, /* ByteArray is specific to RabbitMQ */
+    ///! No value
     Void,
 }
 
 impl AMQPType {
+    ///! Get the AMQPType corresponding to the given id.
+    ///! We don't strictly follow the spec here but rather the RabbitMQ implementation
+    ///! 's' means ShortInt (like 'U') instead of ShortString
+    ///! 'l' and 'L' both mean LongLongInt (no LongLongUInt)
     pub fn from_id(id: char) -> Option<AMQPType> {
         match id {
             't' => Some(AMQPType::Boolean),
@@ -54,6 +78,11 @@ impl AMQPType {
         }
     }
 
+    ///! Get the id from an AMQPType
+    ///! We don't strictly follow the spec here but rather the RabbitMQ implementation
+    ///! ShortString doesn't have an id, we return '_' instead
+    ///! ShortInt is supposed to be 'U' but we use 's'
+    ///! LongLongUInt is supposed to be 'L' but we return 'l' as LongLongInt
     pub fn get_id(&self) -> char {
         match *self {
             AMQPType::Boolean        => 't',
@@ -80,10 +109,6 @@ impl AMQPType {
             AMQPType::Void           => 'V',
         }
     }
-
-    pub fn to_string(&self) -> String {
-        format!("{}", self)
-    }
 }
 
 impl fmt::Display for AMQPType {
@@ -92,28 +117,49 @@ impl fmt::Display for AMQPType {
     }
 }
 
+///! A bool
 pub type Boolean        = bool;
+///! An i8
 pub type ShortShortInt  = i8;
+///! A u8
 pub type ShortShortUInt = u8;
+///! An i16
 pub type ShortInt       = i16;
+///! A u16
 pub type ShortUInt      = u16;
+///! An i32
 pub type LongInt        = i32;
+///! A u32
 pub type LongUInt       = u32;
+///! An i64
 pub type LongLongInt    = i64;
+///! A u64
 pub type LongLongUInt   = u64;
+///! A f32
 pub type Float          = f32;
+///! A f64
 pub type Double         = f64;
+///! A String (deprecated)
 pub type ShortString    = String;
+///! A String
 pub type LongString     = String;
+///! An array of AMQPValue
 pub type FieldArray     = Vec<AMQPValue>;
+///! A timestamp (u32)
 pub type Timestamp      = LongLongUInt;
+///! A Map<String, AMQPValue>
 pub type FieldTable     = BTreeMap<ShortString, AMQPValue>;
+///! An array of bytes (RabbitMQ specific)
 pub type ByteArray      = Vec<u8>;
+///! No value
 pub type Void           = ();
 
+///! A Decimal value composed of a scale and a value
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct DecimalValue {
+    ///! The scale of the value
     pub scale: ShortShortUInt,
+    ///! The actual value
     pub value: LongUInt,
 }
 
