28b1dba4631e3199d3be5384a44f96e5d309a7d6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-06-22 11:46:45 +0200

    types: add crate documentation
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,5 +1,13 @@
 #![deny(missing_docs)]
 
+//! # AMQP types manipulation library
+//!
+//! amq-protocol-types is a library aiming at providing an implementation/abstraction
+//! around AMQP types.
+//!
+//! It implements the list of the different kind of types available, a value holder and
+//! serialization.deserialization facilities.
+
 #[macro_use] extern crate cookie_factory;
 #[macro_use] extern crate nom;
 extern crate serde;
