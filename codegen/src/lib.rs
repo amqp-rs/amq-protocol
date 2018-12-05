045b3f95ad52caeb685578e65a0779a4455695c3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2018-12-05 15:15:27 +0100

    codegen: v1.0.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(missing_docs)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/1.0.0/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/1.0.1/")]
 
 //! # AMQP code generation utilities
 //!
