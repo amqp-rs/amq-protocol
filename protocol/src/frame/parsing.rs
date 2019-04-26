6f50d6d11cc2e2029f6641f8e8168023000a7ff8
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-04-26 11:01:31 +0200

    protocol: port to nom5
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/src/frame/parsing.rs
+++ protocol/src/frame/parsing.rs
@@ -1,63 +1,55 @@
 use nom;
 
-use crate::frame::*;
-use crate::protocol::*;
-use crate::protocol::basic::parse_properties;
-use crate::types::parsing::*;
+use crate::{
+    frame::*,
+    protocol::*,
+    protocol::basic::parse_properties,
+    types::parsing::*,
+};
 
-use nom::*;
+use nom::{
+    bytes::complete::{tag, take},
+    combinator::{flat_map, map, map_opt, map_res},
+};
 
-named_attr!(#[doc = "Parse a channel id"], pub parse_channel<AMQPChannel>, map!(parse_id, From::from));
-
-named_attr!(#[doc = "Parse the protocol header"], pub parse_protocol_header, do_parse!(
-    tag!(metadata::NAME.as_bytes())                                                        >>
-    tag!(&[0])                                                                             >>
-    version: tag!(&[metadata::MAJOR_VERSION, metadata::MINOR_VERSION, metadata::REVISION]) >>
-    (version)
-));
-
-named_attr!(#[doc = "Parse the frame type"], pub parse_frame_type<AMQPFrameType>, switch!(parse_short_short_uint,
-    constants::FRAME_METHOD    => value!(AMQPFrameType::Method) |
-    constants::FRAME_HEADER    => value!(AMQPFrameType::Header) |
-    constants::FRAME_BODY      => value!(AMQPFrameType::Body)   |
-    constants::FRAME_HEARTBEAT => value!(AMQPFrameType::Heartbeat)
-));
-
-/// Parse a full AMQP Frame (with contents)
-pub fn parse_frame(i: &[u8]) -> Result<(&[u8], AMQPFrame), nom::Err<&[u8]>> {
-    let (remaining, raw) = try_parse!(i, parse_raw_frame);
-    let (_, frame)       = match raw.frame_type {
-        AMQPFrameType::Method    => try_parse!(raw.payload, map!(parse_class,          |m: AMQPClass|         AMQPFrame::Method(raw.channel_id, m))),
-        AMQPFrameType::Header    => try_parse!(raw.payload, map!(parse_content_header, |h: AMQPContentHeader| AMQPFrame::Header(raw.channel_id, h.class_id, Box::new(h)))),
-        AMQPFrameType::Body      => (remaining, AMQPFrame::Body(raw.channel_id, Vec::from(raw.payload))),
-        AMQPFrameType::Heartbeat => (remaining, AMQPFrame::Heartbeat(raw.channel_id)),
-    };
-    Ok((remaining, frame))
+/// Parse a channel id
+pub fn parse_channel(i: &[u8]) -> ParserResult<'_, AMQPChannel> {
+    map(parse_id, From::from)(i)
 }
 
-named_attr!(#[doc = "Parse a raw AMQP frame"], pub parse_raw_frame<AMQPRawFrame<'_>>, do_parse!(
-    frame:   parse_frame_type     >>
-    channel: parse_id             >>
-    size:    parse_long_uint      >>
-    payload: take!(size)          >>
-    tag!(&[constants::FRAME_END]) >>
-    (AMQPRawFrame {
-        frame_type: frame,
-        channel_id: channel,
-        size,
-        payload,
-    })
-));
+/// Parse the protocol header
+pub fn parse_protocol_header(i: &[u8]) -> ParserResult<'_, ()> {
+    map(flat_map(tag(metadata::NAME.as_bytes()), |_| flat_map(tag(&[0]), |_| tag(&[metadata::MAJOR_VERSION, metadata::MINOR_VERSION, metadata::REVISION]))), |_| ())(i)
+}
 
-named_attr!(#[doc = "Parse a content header frame"], pub parse_content_header<AMQPContentHeader>, do_parse!(
-    class:      parse_id             >>
-    weight:     parse_short_uint     >>
-    size:       parse_long_long_uint >>
-    properties: parse_properties     >>
-    (AMQPContentHeader {
-        class_id:  class,
-        weight,
-        body_size: size,
-        properties,
-    })
-));
+/// Parse the frame type
+pub fn parse_frame_type(i: &[u8]) -> ParserResult<'_, AMQPFrameType> {
+    map_opt(parse_short_short_uint, |method| match method {
+        constants::FRAME_METHOD    => Some(AMQPFrameType::Method),
+        constants::FRAME_HEADER    => Some(AMQPFrameType::Header),
+        constants::FRAME_BODY      => Some(AMQPFrameType::Body),
+        constants::FRAME_HEARTBEAT => Some(AMQPFrameType::Heartbeat),
+        _                          => None,
+    })(i)
+}
+
+/// Parse a full AMQP Frame (with contents)
+pub fn parse_frame(i: &[u8]) -> ParserResult<'_, AMQPFrame> {
+    map_res(parse_raw_frame, |raw| match raw.frame_type {
+        // FIXME: check EOF
+        AMQPFrameType::Method    => parse_class(raw.payload).map(|(_, m)| AMQPFrame::Method(raw.channel_id, m)),
+        AMQPFrameType::Header    => parse_content_header(raw.payload).map(|(_, h)| AMQPFrame::Header(raw.channel_id, h.class_id, Box::new(h))),
+        AMQPFrameType::Body      => Ok(AMQPFrame::Body(raw.channel_id, Vec::from(raw.payload))),
+        AMQPFrameType::Heartbeat => Ok(AMQPFrame::Heartbeat(raw.channel_id)),
+    })(i)
+}
+
+/// Parse a raw AMQP frame
+pub fn parse_raw_frame(i: &[u8]) -> ParserResult<'_, AMQPRawFrame<'_>> {
+    flat_map(parse_frame_type, |frame_type| flat_map(parse_id, move |channel_id| flat_map(parse_long_uint, move |size| flat_map(take(size), move |payload| map(tag(&[constants::FRAME_END]), move |_| AMQPRawFrame { frame_type, channel_id, size, payload })))))(i)
+}
+
+/// Parse a content header frame
+pub fn parse_content_header(i: &[u8]) -> ParserResult<'_, AMQPContentHeader> {
+    flat_map(parse_id, |class_id| flat_map(parse_short_uint, move |weight| flat_map(parse_long_long_uint, move |body_size| map(parse_properties, move |properties| AMQPContentHeader { class_id, weight, body_size, properties }))))(i)
+}
