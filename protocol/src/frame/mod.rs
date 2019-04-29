246310be2e03de9b9e34dd5097bc88963e07feec
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-04-29 11:02:09 +0200

    protocol: more reexports
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/src/frame/mod.rs
+++ protocol/src/frame/mod.rs
@@ -4,6 +4,9 @@ pub use self::generation::gen_frame;
 pub use self::parsing::parse_frame;
 pub use self::structs::*;
 
+pub use cookie_factory::GenError;
+pub use nom::Offset;
+
 /// Serialization utils
 pub mod generation;
 /// Parsing utils
