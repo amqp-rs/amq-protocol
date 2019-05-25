320927fed7d8f3b99bda4a4660f469e7c46dfbb3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-05-25 23:19:10 +0200

    v2.0.0-rc10
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,14 +1,12 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol/2.0.0-rc9/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol/2.0.0-rc10/")]
 
 //! # AMQP manipulation library
 //!
 //! amq-protocol is a library aiming at providing tools to help
 //! implementing software using AMQP
 
-/// Reexport of amq_protocol_codegen
-pub mod codegen;
 /// AMQP Frame handling utils
 pub mod frame;
 /// The AMQ Protocol implementation (Generated)
--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-tcp/2.0.0-rc9/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-tcp/2.0.0-rc10/")]
 
 //! # AMQP URI TCP connection handling
 //!
