5e9a4f3a841de70cd972d262395365c885a79eb6
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-06-26 13:23:07 +0200

    v2.1.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-types/2.0.2/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-types/2.1.0/")]
 
 //! # AMQP types manipulation library
 //!
