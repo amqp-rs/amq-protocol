2e50b6263238cc33b33daf6b5107ad6d3539838d
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-08-08 17:44:37 +0200

    v2.2.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol/2.1.0/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol/2.2.0/")]
 
 //! # AMQP manipulation library
 //!
