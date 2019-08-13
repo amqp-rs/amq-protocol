9ec3df168e65a3db5b4d8c7db6ead8570f3182bc
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2019-08-13 11:34:25 +0300

    v3.0.0-beta.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/2.2.0/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/3.0.0-beta.1/")]
 
 //! # AMQP code generation utilities
 //!
