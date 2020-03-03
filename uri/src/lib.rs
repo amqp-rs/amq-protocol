e07bd9831abf111cb2f9fce7879159c5317e2c1b
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-03-03 13:18:03 +0100

    v5.0.0-beta.3
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- uri/src/lib.rs
+++ uri/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/5.0.0-beta.2/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/5.0.0-beta.3/")]
 
 //! # AMQP URI manipulation library
 //!
