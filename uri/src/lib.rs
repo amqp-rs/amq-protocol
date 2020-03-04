3c72c4bae2bd9a8065d57d4e7a4e0486995681ac
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-03-04 10:06:57 +0100

    v4.2.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- uri/src/lib.rs
+++ uri/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/4.2.0/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/4.2.1/")]
 
 //! # AMQP URI manipulation library
 //!
