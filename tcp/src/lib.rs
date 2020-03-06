377daf3db7c7fdfd8faf848b33ba648b1c438e2b
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-03-06 10:17:24 +0100

    v5.0.0
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-tcp/5.0.0-beta.4/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-tcp/5.0.0/")]
 
 //! # AMQP URI TCP connection handling
 //!
