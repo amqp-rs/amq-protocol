d4c4dc944773473482f9f8b1184b3f69ee672f54
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-04-20 16:57:44 +0200

    v6.0.0-beta1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/6.0.0-alpha6/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/6.0.0-beta1/")]
 
 //! # AMQP code generation utilities
 //!
--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-types/6.0.0-alpha6/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-types/6.0.0-beta1/")]
 
 //! # AMQP types manipulation library
 //!
