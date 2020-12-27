38a6b29f0fffc4cfca387ec72af87a699186cd88
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-12-27 12:36:00 +0100

    v7.0.0-alpha.5
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-types/7.0.0-alpha.4/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-types/7.0.0-alpha.5/")]
 
 //! # AMQP types manipulation library
 //!
--- uri/src/lib.rs
+++ uri/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/7.0.0-alpha.4/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/7.0.0-alpha.5/")]
 
 //! # AMQP URI manipulation library
 //!
