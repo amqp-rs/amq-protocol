546d60744c69f8dbf4fff80ad65d8057bd78a77b
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-01-04 13:47:34 +0100

    pass edition to rustfmt
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- regen-code.sh
+++ regen-code.sh
@@ -5,7 +5,7 @@ main() {
     export AMQ_PROTOCOL_CODEGEN_FILE="generated"
 
     cargo build --manifest-path protocol/Cargo.toml --features=codegen-internal
-    rustfmt "${AMQ_PROTOCOL_CODEGEN_DIR}/${AMQ_PROTOCOL_CODEGEN_FILE}.rs"
+    rustfmt --edition=2021 "${AMQ_PROTOCOL_CODEGEN_DIR}/${AMQ_PROTOCOL_CODEGEN_FILE}.rs"
 }
 
 main "${@}"
