a3e69f2ddfcf5252df09cafcaa91dc157e886e0e
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2022-02-24 15:24:50 +0100

    v7.0.1
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/7.0.0/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-codegen/7.0.1/")]
 
 //! # AMQP code generation utilities
 //!
