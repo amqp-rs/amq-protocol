c021ceb958d899fa573f2fada7ba744f84dd86b4
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2023-05-19 13:51:39 +0200

    v7.1.2
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- uri/src/lib.rs
+++ uri/src/lib.rs
@@ -1,6 +1,6 @@
 #![deny(missing_docs)]
 #![warn(rust_2018_idioms)]
-#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/7.1.1/")]
+#![doc(html_root_url = "https://docs.rs/amq-protocol-uri/7.1.2/")]
 
 //! # AMQP URI manipulation library
 //!
