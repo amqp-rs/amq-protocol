7b4daa99287b4c6b4eabf59b222ed85ee54f8aa0
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-08-22 22:03:00 +0200

    tcp: switch to async-rs

--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -7,11 +7,9 @@
 //! connecting to an AMQP URI
 
 use amq_protocol_uri::{AMQPScheme, AMQPUri};
-use async_trait::async_trait;
+use async_rs::{Runtime, traits::*};
 use cfg_if::cfg_if;
-use executor_trait::BlockingExecutor;
-use reactor_trait::TcpReactor;
-use std::{io, ops::Deref, time::Duration};
+use std::{io, time::Duration};
 use tracing::trace;
 
 /// Re-export TcpStream
@@ -30,7 +28,6 @@ pub use tcp_stream::OpenSslConnector;
 pub use tcp_stream::{RustlsConnector, RustlsConnectorConfig};
 
 /// Trait providing a method to connect to a TcpStream
-#[async_trait]
 pub trait AMQPUriTcpExt {
     /// connect to a TcpStream
     fn connect(&self) -> HandshakeResult
@@ -44,40 +41,31 @@ pub trait AMQPUriTcpExt {
     fn connect_with_config(&self, config: TLSConfig<'_, '_, '_>) -> HandshakeResult;
 
     /// connect to a TcpStream
-    async fn connect_async<R: Deref + Send + Sync, E: Deref + Send + Sync>(
+    fn connect_async<RK: RuntimeKit + Send + Sync>(
         &self,
-        reactor: R,
-        executor: E,
-    ) -> io::Result<AsyncTcpStream>
+        runtime: &Runtime<RK>,
+    ) -> impl Future<Output = io::Result<AsyncTcpStream<<RK as Reactor>::TcpStream>>>
     where
         Self: Sized,
-        R::Target: TcpReactor + Send + Sync,
-        E::Target: BlockingExecutor + Send + Sync,
     {
-        self.connect_with_config_async(TLSConfig::default(), reactor, executor)
-            .await
+        self.connect_with_config_async(TLSConfig::default(), runtime)
     }
 
     /// connect to a TcpStream with the given configuration
-    async fn connect_with_config_async<R: Deref + Send + Sync, E: Deref + Send + Sync>(
+    fn connect_with_config_async<RK: RuntimeKit + Send + Sync>(
         &self,
         config: TLSConfig<'_, '_, '_>,
-        reactor: R,
-        executor: E,
-    ) -> io::Result<AsyncTcpStream>
+        runtime: &Runtime<RK>,
+    ) -> impl Future<Output = io::Result<AsyncTcpStream<<RK as Reactor>::TcpStream>>>
     where
-        R::Target: TcpReactor + Send + Sync,
-        E::Target: BlockingExecutor + Send + Sync;
+        Self: Sized;
 }
 
-#[async_trait]
 impl AMQPUriTcpExt for AMQPUri {
     fn connect_with_config(&self, config: TLSConfig<'_, '_, '_>) -> HandshakeResult {
         cfg_if! {
             if #[cfg(feature = "hickory-dns")] {
-                use hickory_to_socket_addrs::HickoryToSocketAddrs;
-
-                let uri = HickoryToSocketAddrs::new(&self.authority.host, self.authority.port);
+                let uri = async_rs::HickoryToSocketAddrs::new(self.authority.host.clone(), self.authority.port);
             } else {
                 let uri = (self.authority.host.as_str(), self.authority.port);
             }
@@ -96,34 +84,29 @@ impl AMQPUriTcpExt for AMQPUri {
         Ok(stream)
     }
 
-    async fn connect_with_config_async<R: Deref + Send + Sync, E: Deref + Send + Sync>(
+    fn connect_with_config_async<RK: RuntimeKit + Send + Sync>(
         &self,
         config: TLSConfig<'_, '_, '_>,
-        reactor: R,
-        executor: E,
-    ) -> io::Result<AsyncTcpStream>
+        runtime: &Runtime<RK>,
+    ) -> impl Future<Output = io::Result<AsyncTcpStream<<RK as Reactor>::TcpStream>>>
     where
-        R::Target: TcpReactor + Send + Sync,
-        E::Target: BlockingExecutor + Send + Sync,
+        Self: Sized,
     {
         cfg_if! {
             if #[cfg(feature = "hickory-dns")] {
-                use hickory_to_socket_addrs::HickoryToSocketAddrs;
-
-                let uri = HickoryToSocketAddrs::new(self.authority.host.to_owned(), self.authority.port);
-                trace!(uri = ?uri, "Connecting.");
-                drop(executor);
+                let uri = async_rs::HickoryToSocketAddrs::new(self.authority.host.clone(), self.authority.port);
             } else {
-                let uri = (self.authority.host.to_owned(), self.authority.port);
-                trace!(uri = ?uri, "Connecting.");
-                let uri = (executor, uri);
+                let uri = runtime.to_socket_addrs((self.authority.host.clone(), self.authority.port));
             }
         }
-        let stream = AsyncTcpStream::connect(reactor, uri).await?;
-        let stream = match self.scheme {
-            AMQPScheme::AMQP => stream,
-            AMQPScheme::AMQPS => stream.into_tls(&self.authority.host, config).await?,
-        };
-        Ok(stream)
+        trace!(uri = ?uri, "Connecting.");
+        async move {
+            let stream = AsyncTcpStream::connect(runtime, uri).await?;
+            let stream = match self.scheme {
+                AMQPScheme::AMQP => stream,
+                AMQPScheme::AMQPS => stream.into_tls(&self.authority.host, config).await?,
+            };
+            Ok(stream)
+        }
     }
 }
